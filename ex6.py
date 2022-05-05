9633adf115d9806d4351db00fa56adb351b3aab5
Valerii Ilin <v.ilin.liqvid@gmail.com>
2022-05-05 12:16:44 +0300

    Ex6

--- ex6.py
+++ ex6.py
@@ -6,3 +6,7 @@ response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow
 print(response)
 print(response.history)
 print(response.url)
+
+# <Response [403]>
+# [<Response [301]>, <Response [301]>]
+# https://learnqa.ru/
