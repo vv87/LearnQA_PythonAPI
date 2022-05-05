3d2bd912e738344d31b0aecf0c4c3aa2dcf4ce45
Valerii Ilin <v.ilin.liqvid@gmail.com>
2022-05-05 12:15:46 +0300

    Ex6

--- ex6.py
+++ ex6.py
@@ -1,17 +1,8 @@
 import requests
 
 
-response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
 
+print(response)
 print(response.history)
-
-first_response = response.history[0]
-second_response = response.history[1]
-
-print(first_response.url)
-print(second_response.url)
-
-# result:
-# [<Response [301]>, <Response [301]>]
-# https://playground.learnqa.ru/api/long_redirect
-# https://playground.learnqa.ru/
+print(response.url)
