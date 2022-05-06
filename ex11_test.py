684f73e9d748c28bcbe1b9e259fe810d87a57eae
Valerii Ilin <v.ilin.liqvid@gmail.com>
2022-05-06 11:26:25 +0300

    ex11

--- ex11_test.py
+++ ex11_test.py
@@ -6,6 +6,6 @@ def test_cookie():
     homework_cookie = "HomeWork"
     hw_value = "hw_value"
     check_homework_value = url_homework_cookie[homework_cookie]
-    print(check_homework_value)
+    # print(check_homework_value)
 
     assert hw_value in check_homework_value, f'There is no {hw_value} in {homework_cookie} cookie'
