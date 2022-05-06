9ff19bd7cbe31441c0094cda9e7820420f342e9d
Valerii Ilin <v.ilin.liqvid@gmail.com>
2022-05-06 11:25:47 +0300

    refactored ex11

--- ex11_test.py
+++ ex11_test.py
@@ -3,7 +3,9 @@ import requests
 
 def test_cookie():
     url_homework_cookie = requests.get("https://playground.learnqa.ru/api/homework_cookie").cookies
-    check_homework_value = url_homework_cookie["HomeWork"]
+    homework_cookie = "HomeWork"
+    hw_value = "hw_value"
+    check_homework_value = url_homework_cookie[homework_cookie]
     print(check_homework_value)
 
-    assert "hw_value" in check_homework_value, "f'There is no \"hw_value\" in \"HomeWork\" cookie'"
+    assert hw_value in check_homework_value, f'There is no {hw_value} in {homework_cookie} cookie'
