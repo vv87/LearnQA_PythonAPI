b3a8934aa6fdeed330f3873d01a9b48f375dd49b
Valerii Ilin <v.ilin.liqvid@gmail.com>
2022-05-06 11:20:00 +0300

    ex12

---
+++ ex12_test.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def test_header():
+    url_homework_header = requests.get("https://playground.learnqa.ru/api/homework_header").headers
+    home_work_header = "x-secret-homework-header"
+    secret_value = 'Some secret value'
+    check_home_work_value = url_homework_header[home_work_header]
+    # print(url_homework_header)
+
+    assert secret_value in check_home_work_value, f'There is no {secret_value} in {home_work_header}'
