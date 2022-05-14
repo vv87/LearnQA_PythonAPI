c9b4d5e706856b315814e5581856420468556102
Valerii Ilin <v.ilin.liqvid@gmail.com>
2022-05-14 17:40:57 +0300

    ex16

--- lib/assertions.py
+++ lib/assertions.py
@@ -24,6 +24,26 @@ class Assertions:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
     @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_not_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name not in response_as_dict, f"Response JSON should not have key '{name}'. But it's present'"
+
+    @staticmethod
     def assert_code_status(response: Response, expected_status_code):
         assert response.status_code == expected_status_code, \
             f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
@@ -31,3 +51,12 @@ class Assertions:
     @staticmethod
     def assert_response_content_equal_actual_result(response: Response, response_content, assertion_text):
         assert response.content.decode("utf-8") == response_content, assertion_text
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON should not have key '{name}'. But it's present"
---
+++ tests/test_user_get.py
@@ -0,0 +1,63 @@
+import pytest
+import requests
+from LearnQA_PythonAPI.lib.base_case import BaseCase
+from LearnQA_PythonAPI.lib.assertions import Assertions
+
+
+class TestUserGet(BaseCase):
+
+    def test_get_user_details_not_auth(self):
+        response = requests.get("https://playground.learnqa.ru/api/user/2")
+
+        Assertions.assert_json_has_key(response, "username")
+        Assertions.assert_json_has_not_key(response, "email")
+        Assertions.assert_json_has_not_key(response, "firstName")
+        Assertions.assert_json_has_not_key(response, "lastName")
+
+    def test_get_details_auth_as_same_user(self):
+
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        response2 = requests.get(
+            f"https://playground.learnqa.ru/api/user/{user_id_from_auth_method}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        expected_fields = ["username", "email", "firstName", "lastName"]
+
+        Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_details_auth_as_another_user(self):
+
+        data = {
+            "email": "vinkotov@example.com",
+            "password": "1234"
+
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        response2 = requests.get(
+            f"https://playground.learnqa.ru/api/user/544",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_has_key(response2, "username")
+
+        not_expected_fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_not_keys(response2, not_expected_fields)
