08fb7ac67877edde9ce2528c52812b0205c4fbcc
Valerii Ilin <v.ilin.liqvid@gmail.com>
2022-05-16 17:42:38 +0300

    ex17

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -59,55 +59,6 @@ class TestUserEdit(BaseCase):
         self.edit()
         self.get()
 
-        # # REGISTER
-        # register_data = self.prepare_registration_data()
-        # response1 = MyRequests.post("/user/", data=register_data)
-        #
-        # Assertions.assert_code_status(response1, 200)
-        # Assertions.assert_json_has_key(response1, "id")
-        #
-        # email = register_data['email']
-        # first_name = register_data['firstName']
-        # password = register_data['password']
-        # user_id = self.get_json_value(response1, 'id')
-
-        # # LOGIN
-        # login_data = {
-        #     'email': email,
-        #     'password': password
-        # }
-        #
-        # response2 = MyRequests.post("/user/login", data=login_data)
-        #
-        # auth_sid = self.get_cookie(response2, "auth_sid")
-        # token = self.get_header(response2, "x-csrf-token")
-
-        # # EDIT
-        # new_name = "Changed Name"
-        #
-        # response3 = MyRequests.put(
-        #     f"/user/{user_id}",
-        #     headers={"x-csrf-token": token},
-        #     cookies={"auth_sid": auth_sid},
-        #     data={"firstName": new_name}
-        # )
-        #
-        # Assertions.assert_code_status(response3, 200)
-        #
-        # # GET
-        # response4 = MyRequests.get(
-        #     f"/user/{user_id}",
-        #     headers={"x-csrf-token": token},
-        #     cookies={"auth_sid": auth_sid},
-        # )
-        #
-        # Assertions.assert_json_value_by_name(
-        #     response4,
-        #     "firstName",
-        #     new_name,
-        #     "Wrong name of the user after edit"
-        # )
-
     # Попытаемся изменить данные пользователя, будучи неавторизованными
     def test_edit_not_auth_user(self):
         self.register()
